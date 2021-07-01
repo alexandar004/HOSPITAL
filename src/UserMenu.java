b8aaedabfe2d8bf02812a8b96ab08ff36fe27f35
unknown <rogozianina_16@abv.bg>
2021-07-01 21:52:12 +0300

    refactor

--- src/UserMenu.java
+++ src/UserMenu.java
@@ -1,9 +1,4 @@
 import java.util.Scanner;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class UserMenu {
 
@@ -81,24 +76,5 @@ public class UserMenu {
         }
     }
 
-
-    protected static void patientChoice() {
-        var currentLoggedInPatient = Patient.loginAsPatient();
-        if (currentLoggedInPatient != null) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("1 - view all appointments, 2 - update an appointment, 3 - delete an appointment or 4 to turn Back");
-            int input = scanner.nextInt();
-            switch (input) {
-                case 1 -> currentLoggedInPatient.viewAllAppointments();
-                case 2 -> System.out.println("Patient functional 2");
-                case 3 -> System.out.println("Patient functional 3");
-                case 4 -> mainChoice();
-                default -> {
-                    System.out.println("Enter 1, 2, 3 or 4");
-                    patientChoice();
-                }
-            }
-        }
-    }
 
 }
